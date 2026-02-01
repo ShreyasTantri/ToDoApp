1b61c70edec3871fe2928fa22d6c085d9a999fe7
ShreyasTantri <shreyastantrik29@gmail.com>
2026-02-01 11:31:55 +0530

    Initial Commit

---
+++ To-Do/To_DoApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_DoApp.swift
+//  To-Do
+//
+//  Created by CCS038 on 01/02/26.
+//
+
+import SwiftUI
+
+@main
+struct To_DoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
